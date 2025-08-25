0ca5e902975d05c28ca37901052c3234ec89fc99
TyBunPark <133121680+TyBunPark@users.noreply.github.com>
2025-08-25 11:16:41 +0700

    Add files via upload

---
+++ my-static-website/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you! This is a static demo form, it does not send data.');
+});
